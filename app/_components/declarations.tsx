05b32b2cd7768c52af98433022f391ddf47c04ff
Himadri Mandal <87018482+quirtt@users.noreply.github.com>
2024-01-08 04:40:20 +0530

    Edit: add past speakers on pc, needfix: mobile

--- app/_components/declarations.tsx
+++ app/_components/declarations.tsx
@@ -1,2 +1,3 @@
 export const mtrp_jpg: string = '/img/mtrp_ico.jpg'
 export const mtrp_ico: string = '/img/mtrp_ico.ico'
+export const mtrp_white: string = '/img/mtrp_ico_white.png'
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,21 @@
 'use client';
 import React from 'react';
 import 'flowbite';
-import {  mtrp_ico, mtrp_jpg } from './declarations';
+import {  mtrp_ico, mtrp_jpg, mtrp_white } from './declarations';
+import { MouseEventHandler } from 'react';
+
+const RegisterLinkEvent: MouseEventHandler<HTMLButtonElement> = (event) => {
+  window.open('https://forms.gle/qYHpX4Aj9hPceqY17', '_blank');
+};
 
 export default function Nav(){
     return (
       <header className="p-12 text-white">
         <nav className="bg-[#122038] fixed w-full z-20 top-0 start-0 border-b border-gray-200">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <img src={mtrp_jpg} className='flex items-center space-x-3 rtl:space-x-reverse max-w-20 max-h-auto'/>
+          <img src={mtrp_white} className='flex items-center space-x-3 rtl:space-x-reverse max-w-16 max-h-auto'/>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="text-white shadow-lg bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2">LOG IN</button>
+          <button type="button" onClick={RegisterLinkEvent} className="text-white shadow-lg bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2">Register Now</button>
               <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -28,9 +33,6 @@ export default function Nav(){
               </li>
               <li>
                 <a href="#" className="block rounded px-2 py-2 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 border-gray-700 text-white">Winter Camp</a>
-              </li>
-              <li>
-                <a href="#" className="block rounded px-2 py-2 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 border-gray-700 text-white">Registration</a>
               </li>
               <li>
                 <a href="#" className="block rounded px-2 py-2 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 border-gray-700 text-white">Book</a>
