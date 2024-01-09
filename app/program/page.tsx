d3bad9369b7808f544f101f67ff6c134fd332aaf
Himadri Mandal <87018482+quirtt@users.noreply.github.com>
2024-01-10 00:34:02 +0530

    about us, program: COMPLETE

--- app/program/page.tsx
+++ app/program/page.tsx
@@ -23,23 +23,23 @@ export default function Home() {
                 <p className='text-xl pt-2 pb-10 md:text-2xl text-black leading-8 mx-[10%] text-center'>
                 The programme consists of a written examination and a camp. The competition will be held in two categories, one for students studying in or below X (Junior Category) standard and the other for classes XI and XII (Senior Category). Moreover, this time the examination is being held in both online and offline mode in order to extend the availability of this unique experience to the students for whom it will be difficult to report to the designated centres for an offline examination.
                 </p>
-                <p className='text-xl pt-2 pb-10 md:text-2xl text-black text-center leading-8 mx-[10%]'>
+                <p className='text-xl pt-2 pb-10 md:text-2xl text-black text-center leading-8 mx-[4%] md:mx-[10%]'>
                 Students from each category are selected for the math camp based on their performance in the written test. The camp consists of interactive sessions with mathematicians, ISI faculties and scholars from other universities, along with a few exercises. Based on the performance in the camp, the top few students from each category are rewarded.
                 </p>
-                <div className='flex flex-row flex-wrap justify-center gap-4'>
-                    <div className='max-w-[90%] md:max-w-[45%] bg-[#37deff] rounded-[4rem]'>
-                        <div className=" whitespace-pre-line font-bold text-3xl md:text-4xl px-2 pt-2">
-                            <p className='text-[#e6ff42] md:pb-2 text-center'>Level 1</p>
+                <div className='flex flex-row flex-wrap justify-center gap-4 mb-8'>
+                    <div className='max-w-[90%] md:max-w-[45%] bg-[#0D1F35] rounded-[4rem]'>
+                        <div className=" whitespace-pre-line font-bold text-3xl md:text-4xl px-2 pt-4">
+                            <p className='text-[#F1AF57] md:pb-0 text-center'>Level 1</p>
                         </div>
-                        <p className='text-xl pt-2 pb-10 md:text-xl text-[#ff3535] leading-8 mx-[10%] text-center'>
+                        <p className='pt-2 pb-10 md:text-xl text-[#ffffff] leading-8 mx-[4%] md:mx-[10%] text-center'>
                         A written examination is held in various cities in India, specifically, Kolkata, Durgapur, Siliguri separately for the two categories. The corresponding papers consist of two sections. Section I has objective problems (MCQ and/or integer-type) and Section II has subjective problems. The applicants are given two and half hours to solve the paper. Based on their composite scores in the objective and subjective sections, students securing marks above a certain cut-off qualify for the MTRP Camp. The cut-offs are not predetermined and are separate for the Junior and the Senior Category students as well as for the online and offline candidates. The list of selected candidates will be uploaded in the MTRP website and the candidates will also be individually informed.
                         </p>
                     </div>
-                    <div className='max-w-[90%] md:max-w-[45%] bg-[#37deff] rounded-[4rem]'>
-                        <div className=" whitespace-pre-line font-bold text-3xl md:text-4xl px-2 pt-2">
-                            <p className='text-[#e6ff42] md:pb-2 text-center'>Level 2</p>
+                    <div className='max-w-[90%] md:max-w-[45%] bg-[#0D1F35] rounded-[4rem]'>
+                        <div className=" whitespace-pre-line font-bold text-3xl md:text-4xl px-2 pt-4">
+                            <p className='text-[#F1AF57] md:pb-0 text-center'>Level 2</p>
                         </div>
-                        <p className='text-xl pt-2 pb-10 md:text-xl text-[#ff3535] leading-8 mx-[10%] text-center'>
+                        <p className='pt-2 pb-10 md:text-xl text-[#ffffff] leading-8 mx-[4%] md:mx-[10%] text-center'>
                         The selected candidates are required to attend a three day camp. This camp features lectures from mathematicians, ISI faculties, renowned scholars from other universities, and other experts from various fields of Mathematics and Statistics. These lectures are based on interesting topics related to mathematics, thereby providing enthusiasts with lots of food for thought. Throughout the camp, quizzes are held periodically to judge the skills and problem-solving potential of the students. Based on their performance in the camp, the top few students are rewarded (separate for online and offline candidates).
                         </p>
                     </div>
