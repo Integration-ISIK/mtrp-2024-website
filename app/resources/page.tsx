b997e5ff75cd5c8f3d8e2168ba2ee4fe1e43bf27
DudeDope <103326710+DudeDope@users.noreply.github.com>
2024-02-10 01:49:16 +0530

    Update page.tsx

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -17,15 +17,6 @@ export default function Resources() {
         <div className="bg-[#15f7ff2d] bg-cover bg-left-top h-auto max-w-screen pt-4">
           <div className="md:p-2">
             <div className=" whitespace-pre-line font-bold text-2xl md:text-3xl px-2">
-              <p className='text-[#3E4154] md:pb-2'>Problem Primer</p>
-            </div>
-            <p className='leading-5 p-2 md:text-2xl'>
-            Students appearing in MTRP 2024 will get the unique opportunity to purchase a limited edition MTRP Problem Primer (Divide & Conquer) published by Integration, Indian Statistical Institute. Handcrafted by a team of RMO and INMO awardees and several students at the Indian Statistical Institute, Kolkata and Bangalore centres, the atypical content of the book will help the talented students in learning mathematics through solving problems in an unorthodox fashion not covered at the high school level. While designed for students appearing for MTRP as a taste of what is in suit for them in the examination, it is also a great resource as a problem repository for several examinations like Olympiads. The book is made available at a relatively nominal price for the amount of content provided, as we strongly believe that learning should be accessible to everybody.
-            </p>
-            <p className='text-xl p-2 pb-6 font-light md:text-2xl'>A sneak peek from the topic Base Systems in Number Theory <a href="https://drive.google.com/file/d/1jsMz3Xw3u4SPMN8wMNdp0daf9HWa1XZ2/view" className='text-red-500'>here!</a></p>
-          </div>
-          <div className="md:p-2">
-            <div className=" whitespace-pre-line font-bold text-2xl md:text-3xl px-2">
               <p className='text-[#3E4154] md:pb-2'>Syllabus</p>
             </div>
             <p className='leading-5 p-2 md:text-2xl'>
@@ -48,7 +39,16 @@ export default function Resources() {
             <p className='text-xl p-2 pb-2 font-light md:text-2xl'>MTRP Senior 2016 question paper <a href="https://drive.google.com/file/d/10ddadKkNk_l03zgbOfZpyOllD-zJoaM4/view?usp=drive_link" className='text-red-500'>here!</a></p>
             <p className='text-xl p-2 pb-2 font-light md:text-2xl'>MTRP Senior 2017 question paper <a href="https://drive.google.com/file/d/114-AdzgJf2uYCDZhFCIvyRYJ3vWec9Aq/view?usp=drive_link" className='text-red-500'>here!</a></p>
             <p className='text-xl p-2 pb-4 font-light md:text-2xl'>MTRP Senior 2018 question paper <a href="https://drive.google.com/file/d/10wzT14aji-O1xMa4ieICI1ixccCx7s6n/view?usp=drive_link" className='text-red-500'>here!</a></p>
-          </div>                
+          </div> 
+          <div className="md:p-2">
+            <div className=" whitespace-pre-line font-bold text-2xl md:text-3xl px-2">
+              <p className='text-[#3E4154] md:pb-2'>Problem Primer</p>
+            </div>
+            <p className='leading-5 p-2 md:text-2xl'>
+            Students appearing in MTRP 2024 will get the unique opportunity to purchase a limited edition MTRP Problem Primer (Divide & Conquer) published by Integration, Indian Statistical Institute. Handcrafted by a team of RMO and INMO awardees and several students at the Indian Statistical Institute, Kolkata and Bangalore centres, the atypical content of the book will help the talented students in learning mathematics through solving problems in an unorthodox fashion not covered at the high school level. While designed for students appearing for MTRP as a taste of what is in suit for them in the examination, it is also a great resource as a problem repository for several examinations like Olympiads. The book is made available at a relatively nominal price for the amount of content provided, as we strongly believe that learning should be accessible to everybody.
+            </p>
+            <p className='text-xl p-2 pb-6 font-light md:text-2xl'>A sneak peek from the topic Base Systems in Number Theory <a href="https://drive.google.com/file/d/1jsMz3Xw3u4SPMN8wMNdp0daf9HWa1XZ2/view" className='text-red-500'>here!</a></p>
+          </div>          
         </div>
       </main>
 
